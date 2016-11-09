12aaecc99b27d0b3f2510fca860585dee240df6b
linker85 <linker85@hotmail.com>
2016-11-08 20:00:08 -0500

    Create an app with a ViewPager and a TabLayout with at least 3 different Fragments && Create an app that hides the toolbar on scroll

---
+++ app/src/main/java/com/example/com/homeworkw4d2/ScrollingActivity.java
@@ -0,0 +1,19 @@
+package com.example.com.homeworkw4d2;
+
+import android.os.Bundle;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    private NestedScrollView scroll;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
